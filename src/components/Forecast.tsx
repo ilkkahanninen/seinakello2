7b747f12bd96b4eb785a6a54aa80fc457804d9d0
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2019-11-10 10:18:22 +0200

    Ennusteet riveittäin

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -3,9 +3,9 @@ import styled from "styled-components"
 import { useForecast, ForecastData, ForecastValue } from "../hooks/forecast"
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 60px;
+  max-width: 600px;
+  margin: 0 auto;
+  margin-top: 50px;
 `
 
 export const Forecast: React.FC = () => {
@@ -15,7 +15,7 @@ export const Forecast: React.FC = () => {
 
   return (
     <Container>
-      <ForecastPart data={forecast.Day} temperature={forecast.Temperature.Maximum} label="Tänään" />
+      <ForecastPart data={forecast.Day} temperature={forecast.Temperature.Maximum} label="Päivällä" />
       <ForecastPart data={forecast.Night} temperature={forecast.Temperature.Minimum} label="Yöllä" />
     </Container>
   )
@@ -24,25 +24,35 @@ export const Forecast: React.FC = () => {
 // ----------
 
 const PartContainer = styled.div`
-  width: 200px;
-  text-align: left;
+  display: flex;
+  align-items: center;
+`
+
+const ForecastLabel = styled.div`
+  width: 12%;
+`
+
+const WeatherIconContainer = styled.div`
+  width: 15%;
 `
 
 const WeatherIcon = styled.img`
   display: block;
-  width: 50%;
-  height: auto;
-  margin: 40px 0;
+  height: 64px;
+  width: auto;
 `
 
 const Temperature = styled.span`
-  font-size: 64px;
+  font-size: 48px;
+  width: 23%;
 `
 
 const TemperatureUnit = styled.span`
 `
 
-const Description = styled.div``
+const Description = styled.div`
+  padding-left: 10px;
+`
 
 interface ForecastPartProps {
   data: ForecastData;
@@ -53,10 +63,14 @@ interface ForecastPartProps {
 const ForecastPart = ({ data, temperature, label }: ForecastPartProps) => {
   return (
     <PartContainer>
-      <div>{label}</div>
-      <WeatherIcon src={`https://www.accuweather.com/images/weathericons/${data.Icon}.svg`} />
-      <Temperature>{temperature.Value}°</Temperature>
-      <TemperatureUnit>{temperature.Unit}</TemperatureUnit>
+      <ForecastLabel>{label}</ForecastLabel>
+      <WeatherIconContainer>
+        <WeatherIcon src={`https://www.accuweather.com/images/weathericons/${data.Icon}.svg`} />
+      </WeatherIconContainer>
+      <Temperature>
+        {temperature.Value}°
+        <TemperatureUnit>{temperature.Unit}</TemperatureUnit>
+      </Temperature>
       <Description>{data.LongPhrase}</Description>
     </PartContainer>
   )
